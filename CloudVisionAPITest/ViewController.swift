2f1a910aedc03ac6e6871259472fac2712de4a86
Miki Nomura <miki@fyi.tokyo>
2017-11-03 18:00:02 +0900

    First Commit

--- CloudVisionAPITest/ViewController.swift
+++ CloudVisionAPITest/ViewController.swift
@@ -7,9 +7,32 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var text: UILabel!
+    
+    @IBAction func selectPhoto(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            //Reset texts
+            text.text = ""
+            
+            //Pick an image
+            let picker = UIImagePickerController()
+            picker.sourceType = .photoLibrary
+            
+            //No Edit
+            picker.allowsEditing = false
+            
+            //Show Cameraroll
+            picker.delegate = self
+            present(picker, animated: true, completion: nil)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +42,78 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let selected = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        image.contentMode = .scaleAspectFit
+        image.image = selected
+        
+        text.text = "Detecting texts..."
+        
+        dismiss(animated: true, completion: nil)
+        
+        detectText()
+    }
+    
+    enum DetectMethod: String {
+        case GOOGLE
+    }
+    
+    var method = DetectMethod.GOOGLE
+    
+    func detectText() {
+        switch method {
+        case DetectMethod.GOOGLE:
+            detectTextGoogle()
+        }
+    }
+    
+    func detectTextGoogle() {
+        if let base64image = UIImagePNGRepresentation(image.image!)?.base64EncodedString() {
+            let request: Parameters = [
+                "requests": [
+                    "image": [
+                        "content": base64image
+                    ],
+                    "features": [
+                        [
+                            "type": "TEXT_DETECTION",
+                            "maxResults": 1
+                        ]
+                    ]
+                ]
+            ]
+            
+            let httpHeader: HTTPHeaders = [
+                "Content-Type": "application/json",
+                "X-Ios-Bundle-Identifier": Bundle.main.bundleIdentifier ?? ""
+            ]
+            
+            var googleApiKey = "AIzaSyBK7YdH1J5k6eA1GbPjRnczcPHfLgpnqiY"
+            
+            Alamofire.request("https://vision.googleapis.com/v1/images:annotate?key=\(googleApiKey)", method: .post, parameters: request, encoding: JSONEncoding.default, headers: httpHeader).validate(statusCode: 200..<300).responseJSON { response in
+                // レスポンスの処理
+                self.googleResult(response: response)
+            }
+        }
+    }
+    
+    func googleResult(response: DataResponse<Any>) {
+        guard let result = response.result.value else {
+            // レスポンスが空っぽだったりしたら終了
+            return
+        }
+        let json = JSON(result)
+        let annotations: JSON = json["responses"][0]["textAnnotations"]
+        var detectedText: String = ""
+        // 結果からdescriptionを取り出して一つの文字列にする
+        annotations.forEach { (_, annotation) in
+            detectedText += annotation["description"].string!
+        }
+        // 結果を表示する
+        text.text = detectedText
+    }
 
 }
 
